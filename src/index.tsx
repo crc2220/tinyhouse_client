85153b2eb8a68fdb7a8b4c3c4e762be5bc1aa73f
Chris Carey <carey_christopher@bah.com>
2020-06-01 19:18:45 -0400

    updating with basic routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,39 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import { Listings } from "./sections";
+import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
 import * as serviceWorker from "./serviceWorker";
-import "./styles/index.css"
+import "./styles/index.css";
 // Apollo Boost has pre-built config for caching, state management, and error handling
 const client = new ApolloClient({
     // since we're proxying localhost:9000 in package.json we can just do "/api"
-    uri: "/api"
+    uri: "/api",
 });
 
-render(
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                {/* you want `exact` in the route to avoid partial matching pitfalls when using Switch  */}
+                <Route exact path="/" component={Home} />
+                <Route exact path="/host" component={Host} />
+                <Route exact path="/listing/:id" component={Listing} />
+                <Route exact path="/listings/:location?" component={Listings} />
+                <Route exact path="/user/:id" component={User} />
+                <Route component={NotFound} />
+            </Switch>
+        </Router>
+    );
+};
 
+render(
     <ApolloProvider client={client}>
-        <Listings title="TinyHouse Listings" />
-    </ApolloProvider>, 
+        <App />
+    </ApolloProvider>,
 
     document.getElementById("root")
-
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/sections/Listings/components/ListingsSkeleton/ListingsSkeleton.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Skeleton } from "antd";
-
-export const ListingsSkeleton = () => {
-    return
-};
